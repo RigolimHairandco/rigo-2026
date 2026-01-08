29e4431b07366057ab9a194527d274fc6fd00174
rigolimhairandco <andercleidesigner@gmail.com>
2026-01-08 17:20:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ st.markdown("""
 @st.cache_data
 def load_data():
     # --- CARREGAMENTO PRINCIPAL (CLIENTES) ---
-    file_path = 'todos_os_clientes_30_12_2025.csv'
+    file_path = 'todos_os_30_12_2025.csv'
     try:
         df = pd.read_csv(file_path, sep=';', encoding='utf-8')
     except:
